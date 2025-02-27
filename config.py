dfb54fe38c83dbf2929e2f173e92b1347e428954
mendax <dajnoven@gmail.com>
2025-02-27 21:55:25 +0200

    Fix 27.02.2025

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv('key.env')
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    EXCEL_FILE = "app/data/products.xlsx"
+    SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret_key')
+    SECRET_PASS = os.getenv('SECRET_PASS', 'fallback_secret_pass')
+
+    # Настройки почты для Gmail
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'dajnoven@gmail.com'
+    MAIL_PASSWORD = SECRET_PASS
+    MAIL_DEFAULT_SENDER = 'dajnoven@gmail.com'
